72a50f01b99403293a3f7316edbb3dba9f2fb839
Atta Rehman <ranaatta844@gmail.com>
2023-04-05 09:54:34 +0000

    finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -122,6 +122,25 @@ class Dev(Configuration):
         },
     ]
 
+    #setting the Logiing for the APP
+    LOGGING = {
+  "version": 1,
+  "disable_existing_loggers": False,
+  "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        },
+  "handlers": {
+      "console": {"class": "logging.StreamHandler", "stream": "ext://sys.stdout","formatter": "verbose",},
+  },
+  "root": {
+      "handlers": ["console"],
+      "level": "DEBUG",
+  }
+}
+
 
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
