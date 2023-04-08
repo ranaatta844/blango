d950327a6984d4259044a95070529de5b37469db
Atta Rehman <ranaatta844@gmail.com>
2023-04-08 08:23:17 +0000

    finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django import template
 from django.utils.html import format_html
 from blog.models import Post
+import logging
 
+logger= logging.getLogger(__name__)
 register = template.Library()
 
 #creating the custom filter to check and display author on html page.
@@ -65,4 +67,5 @@ def endcol():
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {'title':'Recent Posts','posts':posts}
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
+#from django.views.decorators.cache import cache_page
+#from django.views.decorators.vary import vary_on_cookie
+
 logger= logging.getLogger(__name__)
 # Create your views here.
 
