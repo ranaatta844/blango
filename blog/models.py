44fc25f7c60b6424531f720b7c8632b47b13c167
Atta Rehman <ranaatta844@gmail.com>
2023-05-12 10:29:01 +0000

    Finish related fields

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Comment(models.Model):
 
 # Creating the tag model Table
 class Tag(models.Model):
-  value = models.TextField(max_length = 100)
+  value = models.TextField(max_length = 100, unique= True)
   def __str__(self):
     return self.value
     
